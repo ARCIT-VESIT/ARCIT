e4e330c8ec4b4620735a9115f5521fc9d10c3629
DESKTOP-SBR0LFR\Nitin <sahunitin1999@gmail.com>
2020-11-21 12:24:58 +0530

    merge

--- ARCIT/views.py
+++ ARCIT/views.py
@@ -1,12 +1,4 @@
 from django.contrib.auth import login, authenticate
-<<<<<<< HEAD
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-=======
 from django.shortcuts import render, redirect
 
 from ARCIT.core.forms import SignUpForm
@@ -14,7 +6,6 @@ from ARCIT.core.forms import SignUpForm
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
->>>>>>> 8ef0257d2ad29b0b7d97beaf13e6a50cd46e3ddb
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -23,9 +14,5 @@ def signup(request):
             login(request, user)
             return redirect('home')
     else:
-<<<<<<< HEAD
-        form = UserCreationForm()
-=======
         form = SignUpForm()
->>>>>>> 8ef0257d2ad29b0b7d97beaf13e6a50cd46e3ddb
     return render(request, 'signup.html', {'form': form})
