f2141ea8c2cbd1ba12c4617b2746dce6f656d46b
DESKTOP-SBR0LFR\Nitin <sahunitin1999@gmail.com>
2020-11-24 15:34:57 +0530

    added patient registration

--- ARCIT/urls.py
+++ ARCIT/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 from ARCIT import views as core_views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +26,5 @@ urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name='logged_out.html'), name='logout'),
     url(r'^signup/$', core_views.signup, name='signup'),
+    path('', include('patregistration.urls')),
 ]
---
+++ patregistration/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PatregistrationConfig(AppConfig):
+    name = 'patregistration'
