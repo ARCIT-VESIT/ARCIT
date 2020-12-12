db4428763b9e2758d9a98f3a380ec97532519f56
Sohail Ansari <ansarisohail014@gmail.com>
2020-12-12 13:12:28 +0530

    merged

--- Doctor/urls.py
+++ Doctor/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from Doctor.views import DoctorView
+from Doctor.views import DoctorView,ViewDocotrProfile
 
 urlpatterns = [
     url(r'^docreg/$', DoctorView.as_view(template_name='doc_reg.html'), name='docreg'),
+    url(r'^d/profile/$', ViewDocotrProfile.as_view(template_name='Doctor/profile.html'), name='doctorprofile'),
 ]
